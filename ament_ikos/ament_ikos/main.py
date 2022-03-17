b4ea2bc76b21dbe36e5499730dc17ba60aa80901
Michael Jeronimo <michael.jeronimo@openrobotics.org>
2022-03-17 10:46:53 -0700

    Move code into separate report aggregation functions
    Signed-off-by: Michael Jeronimo <michael.jeronimo@openrobotics.org>

--- ament_ikos/ament_ikos/main.py
+++ ament_ikos/ament_ikos/main.py
@@ -28,8 +28,7 @@ IKOS_DB_FILE_EXT = '.ikosdb'
 
 
 def indent(elem, level=0):
-    """A low-budget way to format the XML to avoid bringing in another library (stack overflow #3095434)."""
-
+    """Use a low-budget method to format the XML to avoid bringing in another library (stack overflow #3095434)."""
     i = '\n' + level * '  '
     if len(elem):
         if not elem.text or not elem.text.strip():
@@ -47,7 +46,6 @@ def indent(elem, level=0):
 
 def scan_marker_files(directory: str) -> List[pathlib.Path]:
     """Return a list of marker files in a target directory."""
-
     def remove_cmake_compiler_test_files(path: pathlib.Path) -> bool:
         if '/CMakeFiles/' in str(path):
             return False
@@ -59,7 +57,6 @@ def scan_marker_files(directory: str) -> List[pathlib.Path]:
 
 def run_ikos(bitcode_path, ikos_db_path):
     """Run an IKOS analysis on a bitcode file."""
-
     # IKOS doesn't provide the report on stdout if there are > 15 items. So, use "--format no" here
     # to avoid generating a report to stdout here and we'll do it later using a separate ikos-report
     cmd = ['ikos', bitcode_path, '-o', ikos_db_path, '-q', '--format', 'no']
@@ -76,17 +73,15 @@ def run_ikos(bitcode_path, ikos_db_path):
 
 def generate_ikos_stdout(ikos_db_path):
     """Run the IKOS report generator to print the issue list to stdout."""
-
     rc = subprocess.run(['ikos-report', ikos_db_path])
     if rc.returncode != 0:
         pass
 
 
-def generate_ikos_report(ikos_db_path, format='junit', format_ext='junit.xml'):
+def generate_ikos_report(ikos_db_path, fmt='junit', format_ext='junit.xml'):
     """Generate an IKOS report in one of the IKOS-supported formats (we use JUnit XML and SARIF)."""
-
     report_filename = f'{os.path.splitext(ikos_db_path)[0]}.{format_ext}'
-    cmd = ['ikos-report', '--format', format, '--report-file', report_filename]
+    cmd = ['ikos-report', '--format', fmt, '--report-file', report_filename]
 
     # The target database must be the last argument, after the flags
     cmd.append(ikos_db_path)
@@ -98,7 +93,6 @@ def generate_ikos_report(ikos_db_path, format='junit', format_ext='junit.xml'):
 
 def process_marker_file(marker_file, args):
     """Generate the stdout as well as optional JUnit XML and SARIF output files."""
-
     with marker_file.open() as jsonf:
         data = json.load(jsonf)
         bc_path = data['bc']
@@ -117,6 +111,59 @@ def process_marker_file(marker_file, args):
             return ikos_db_path
         else:
             print('Cannot generate report for ' + bc_path + ' due to analysis failure.')
+
+
+def aggregate_junit_xml_files(ikos_db_filenames, summary_filename, summary_name):
+    """Aggregate the JUnit XML files for each test into a single ikos.junit.xml file."""
+    with open(summary_filename, 'wb') as xunit_file:
+        # Wrap all of the concatenated XML output files with a <testsuites> element
+        top = ET.Element('testsuites')
+
+        # Counters to aggregate the test suite results for all test suites
+        total_tests = 0
+        total_errors = 0
+        total_failures = 0
+        total_time = 0.0
+
+        # Set the attributes for the <testsuites> element
+        top.attrib['name'] = summary_name
+
+        # Handle the file that was output by IKOS for each test
+        for db_filename in ikos_db_filenames:
+            junit_xml_filename = os.path.splitext(db_filename)[0] + '.junit.xml'
+
+            tree = ET.parse(junit_xml_filename)
+            root = tree.getroot()
+
+            # Replace the default testsuite name with the program name that was run under IKOS
+            root.attrib['name'] = os.path.basename(os.path.splitext(db_filename)[0])
+
+            # Work around a bug in the IKOS output where the failures are not reported correctly.
+            # Instead of using the summary value, count the number of failure nodes.
+            root.attrib['failures'] = str(len(root.findall('.//failure')))
+
+            total_tests += int(root.attrib['tests'])
+            total_errors += int(root.attrib['errors'])
+            total_failures += int(root.attrib['failures'])
+            total_time += float(root.attrib['time'])
+
+            # Add the <testsuite> node to the parent <testsuites> node
+            top.append(root)
+
+        # Update summary fields for the top-level <testsuites>
+        top.attrib['tests'] = str(total_tests)
+        top.attrib['errors'] = str(total_errors)
+        top.attrib['failures'] = str(total_failures)
+        top.attrib['time'] = str(total_time)
+
+        indent(top)
+        xunit_file.write(ET.tostring(top, encoding='utf8', method='xml'))
+
+
+def aggregate_sarif_files(ikos_db_filenames, summary_filename, summary_name):
+    """Aggregate the SARIF files for each test into a single ikos.sarif file."""
+    # TODO(mjeronimo): Write a function to aggregate the SARIF files
+    pass
 
 
 def main(argv=sys.argv[1:]) -> int:
@@ -149,53 +196,12 @@ def main(argv=sys.argv[1:]) -> int:
         if result:
             ikos_db_files.append(result)
 
-    # Concatenate the JUnit XML files for each test into a single ikos.junit.xml file
+    # Generate the output files
+    test_name = f'{os.path.basename(args.directory)}.ikos'
     if args.xunit_file:
-        with open(args.xunit_file, 'wb') as xunit_file:
-
-            # Wrap all of the concatenated XML output files with a <testsuites> element
-            top = ET.Element('testsuites')
-
-            # Counters to aggregate the test suite results for all test suites
-            total_tests = 0
-            total_errors = 0
-            total_failures = 0
-            total_time = 0.0
-
-            # Set the attributes for the <testsuites> element
-            top.attrib['name'] = f'{os.path.basename(args.directory)}.ikos'
-
-            # Handle the file that was output by IKOS for each test
-            for db_filename in ikos_db_files:
-                junit_xml_filename = os.path.splitext(db_filename)[0] + '.junit.xml'
-
-                tree = ET.parse(junit_xml_filename)
-                root = tree.getroot()
-
-                # Replace the default testsuite name with the program name that was run under IKOS
-                root.attrib['name'] = os.path.basename(os.path.splitext(db_filename)[0])
-
-                total_tests += int(root.attrib['tests'])
-                total_errors += int(root.attrib['errors'])
-                total_failures += int(root.attrib['failures'])
-                total_time += float(root.attrib['time'])
-
-                # Add the <testsuite> node to the parent <testsuites> node
-                top.append(root)
-
-            # Update summary fields for the top-level <testsuites>
-            top.attrib['tests'] = str(total_tests)
-            top.attrib['errors'] = str(total_errors)
-            top.attrib['failures'] = str(total_failures)
-            top.attrib['time'] = str(total_time)
-
-            indent(top)
-            xunit_file.write(ET.tostring(top, encoding='utf8', method='xml'))
-
-    # Concatenate the SARIF files for each test into a single ikos.sarif file
+        aggregate_junit_xml_files(ikos_db_files, args.xunit_file, test_name)
     if args.xunit_file:
-        # TODO 
-        pass
+        aggregate_sarif_files(ikos_db_files, args.sarif_file, test_name)
 
     return 0
 
